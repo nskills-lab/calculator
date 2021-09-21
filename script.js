72675f7d1c230c7900537a9e6fd19620aa30a0b4
Nina Rey <reynina.w@gmail.com>
2021-09-20 22:09:15 -0600

    Add reset css file, implement logic for displaying input from the calculator

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let num1 = 0;
-let num2 = 0;
+let prevOperand = "";
+let currentOperand = "";
+let prevOperator = "";
+let currentOperator = "";
+let waitingForSecondOperand = true;
+let numbersArr = [];
 let displayWindow = document.querySelector('#display');
 
+
 function add(num1, num2) {
-    return num1 + num2;
+    let result = num1 + num2;
+    console.log(result)
+    return result;
 }
 
 function subtract(num1, num2) {
@@ -22,30 +29,86 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+    console.log(number1);
+    console.log(number2);
 
     switch (operator) {
         case 'add':
-            return add(num1, num2);
+            prevOperand = add(number1, number2);
+            break;
         case 'subtract':
-            return subtract(num1, num2);
+            prevOperand = subtract(number1, number2);
+            break;
         case 'divide':
-            return divide(num1, num2);
+            prevOperand = divide(number1, number2);
+            break;
         case 'multiply':
-            return multiply(num1, num2);
+            prevOperand = multiply(number1, number2);
+            break;
+        default:
+            console.log("Operator did not match");
 
     }
+    currentOperand = "";
+    prevOperator = "";
+    console.log(prevOperand);
+    return prevOperand;
 
 }
 
 function display(e) {
 
     let displayValue = this.textContent;
-    displayWindow.textContent += displayValue;
+    // if (operator === "equal" && (prevOperand === "" || prevOperand === "")) {
+    //     displayWindow.textContent = "ERROR";
+    // }
+
+    if (displayWindow.textContent === '0') {
+        displayWindow.textContent = displayValue;
+    } else {
+        displayWindow.textContent += displayValue;
+    }
+
+    if (prevOperand === "" && currentOperator === "") {
+        prevOperand = displayWindow.textContent;
+    } else {
+        currentOperand = displayWindow.textContent
+    }
+
+}
+
+function handleOperation(e) {
+
+    if (prevOperator === "") {
+        prevOperator = this.getAttribute('value');
+
+    } else {
+        currentOperator = this.getAttribute('value');
+
+    }
+
+
+
+    displayWindow.textContent = '';
+    console.log(currentOperand);
+    console.log(prevOperand);
+    if (currentOperator !== "" && currentOperand !== "") {
+        displayWindow.textContent = operate(prevOperator, prevOperand, currentOperand);
+        console.log(displayWindow.textContent);
+
+    }
+
 
 }
 const numbers = document.querySelectorAll('.numbers');
-console.log(numbers);
+const operators = document.querySelectorAll('.operators');
+
 numbers.forEach((number) => {
-    console.log('in display method');
     number.addEventListener('click', display);
 })
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', handleOperation);
+});
